bc28317ab3fac64009f8bdb99cc2964b20d127a2
LukasZvikas <lzvikas1@gmail.com>
2019-03-29 22:05:49 -0400

    Task: deploying to heroku

--- server/app.js
+++ server/app.js
@@ -3,15 +3,20 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
 const { isAuth } = require("./services/isAuth");
+const serveStatic = require('serve-static');
 const cors = require("cors");
 
 mongoose.connect(keys.MONGO_KEY, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(serveStatic(__dirname + "../dist"));
+
 app.use(cors());
 
 app.use(isAuth);
+
+
 
 app.use(bodyParser.json());
 
