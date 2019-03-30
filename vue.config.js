6020ab35d08b97a3aeec2c253ee3e90b8f7708b1
LukasZvikas <lzvikas1@gmail.com>
2019-03-29 23:53:13 -0400

    updated package versions

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   devServer: {
     disableHostCheck: true,
@@ -15,8 +13,7 @@ module.exports = {
       sass: {
         data: `
           @import "@/styles/main.scss";
-        `,
-        includePaths: [path.resolve(__dirname, "node_modules")]
+        `
       }
     }
   }
