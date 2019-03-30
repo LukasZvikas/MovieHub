58a878fcae76d40af37f916454c44ae2b4884ff2
LukasZvikas <lzvikas1@gmail.com>
2019-03-30 00:29:07 -0400

    trying different sass import #2

--- server/app.js
+++ server/app.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
 const { isAuth } = require("./services/isAuth");
-const serveStatic = require('serve-static');
 const cors = require("cors");
 
 mongoose.connect(keys.MONGO_KEY, { useNewUrlParser: true });
 
 const app = express();
-
-app.use(serveStatic(__dirname + "../dist"));
 
 app.use(cors());
 
