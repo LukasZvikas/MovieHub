c6595aeaf7bd2bbed45d64846f47f076de9dfc46
LukasZvikas <lzvikas1@gmail.com>
2019-03-31 00:01:52 -0400

    trying different sass import #24

--- app.js
+++ app.js
@@ -20,4 +20,14 @@ require("./routes/movieRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/dist"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
